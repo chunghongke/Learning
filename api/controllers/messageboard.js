1e008d7ad097e5c086738264173fc1543e1beedb
benson.ke <benson.ke@chunchientaide-MacBook-Pro.local>
2017-10-20 13:12:09 +0800

    form

---
+++ api/controllers/messageboard.js
@@ -0,0 +1,8 @@
+exports.index = function(req,res){
+    res.render('index')
+}
+exports.message = function(req,res){
+    var msg = req.body
+    console.log(msg)
+    res.send('hahahahahaha')
+}
--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-'use strict';
+const bodyParser = require("body-parser");
+const app = require ('express')();
+const port = process.env.port || 10010
+const router = require('./api/controllers/hello_world.js')
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
-module.exports = app; // for testing
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var config = {
-  appRoot: __dirname // required config
-};
-
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
-
-  // install middleware
-  swaggerExpress.register(app);
-
-  var port = process.env.PORT || 10010;
-  app.listen(port);
-
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
-});
+app.get("/",router.index)
+app.post("/message",router.message)
+app.listen(port);
