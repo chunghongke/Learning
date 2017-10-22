23e3b181a82de827eff4a5a6f202dd23b2217600
benson.ke <benson.ke@chunchientaide-MacBook-Pro.local>
2017-10-22 14:50:48 +0800

    add check name format & filter illegal word

--- api/messageboard.js
+++ api/messageboard.js
@@ -1,4 +1,3 @@
-const test = require('./index.js')
 function index(req,res){
     res.render('index')
 }
@@ -6,13 +5,30 @@ function message(req,res){
     var msg = req.body
     res.send(msg)
 }
-function test_email(msg){
+function test_email(mail){
     const email_reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return email_reg.test(msg)
+    return email_reg.test(mail)
 }
-
+function test_name(name){
+    let illegal_word=["fuck","bitch"," "]
+    let num=/\d/
+    let special=/[()[\]+=*&^%$#<>_|~`@!',:;./-]/
+    if(typeof(name) !== 'string')
+        return false
+    for(let val of illegal_word){
+        if(name==val) return false
+    }
+    if(num.test(name)){
+        return false
+    }
+    if(special.test(name)){
+        return false
+    }
+    return true
+}
 module.exports = {
     index,
     message,
-    test_email
+    test_email,
+    test_name
 }
--- test/index.js
+++ test/index.js
@@ -1,12 +1,52 @@
-const msg = require('../api/index.js')
+const msg = require('../api/messageboard.js')
 const assert = require('chai').assert
 describe('test email format', function(){
-
+  it('should be a legal mail-address',()=>{
+    assert(msg.test_email('ano876@gmail.com'))
+  })
   it('should have "@" sign',function(){
-    assert(msg.test_email("234sfdagmail.com"),'should include "@" sign')
+    assert.isFalse(msg.test_email("234sfdagmail.com"),'should include "@" sign')
   })
   it('should not contain special chracter',function(){
-    assert(msg.test_email("Q%#$"),'don\'t use special character')
+    assert.isFalse(msg.test_email("@.."),'don\'t use special character')
+  })
+  it('should not enter a empty string',()=>{
+    assert.isFalse(msg.test_email("  "))
+  })
+  it('should have character before and after "@" sign ',() => {
+    assert.isFalse(msg.test_email("@ssafsadf.casdf"))
+    assert.isFalse(msg.test_email("sfasdf@"))
   })
   
+})
+
+describe('test the legal name',()=>{
+  it('should be a legal name',()=>{
+    assert(msg.test_name('kevin'))
+  })
+  it('should be a string',()=>{
+    assert.isFalse(msg.test_name(2345))
+  })
+  it('should not contain the dirty word',() => {
+    assert.isFalse(msg.test_name('fuck'))
+    assert.isFalse(msg.test_name('bitch'))
+  })
+  it('should not a empty string',()=>{
+    assert.isFalse(msg.test_name(' '))
+  })
+  it('should not contain number',()=>{
+    assert.isFalse(msg.test_name('name1234'))
+  })
+  it('should not contain special character',()=>{
+    assert.isFalse(msg.test_name('`qqwr'))
+    assert.isFalse(msg.test_name('>wr'))
+    assert.isFalse(msg.test_name('@qq<r'))
+    assert.isFalse(msg.test_name('#q.wr'))
+    assert.isFalse(msg.test_name('$q:wr'))
+    assert.isFalse(msg.test_name('%qq\'wr'))
+    assert.isFalse(msg.test_name('^qq;wr'))
+    assert.isFalse(msg.test_name('&qqwr'))
+    assert.isFalse(msg.test_name('|q-wr'))
+    assert.isFalse(msg.test_name('()qqwr'))
+  })
 })
