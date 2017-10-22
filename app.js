c85ee4d24e924cbd7e33b0be3af17d4c7da1192f
benson.ke <benson.ke@chunchientaide-MacBook-Pro.local>
2017-10-22 20:45:19 +0800

    add check post input

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const bodyParser = require("body-parser");
-const app = require ('express')();
+const app = require('express')();
 const port = process.env.port || 10010
 const router = require('./api/messageboard.js')
+module.exports = app;
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",router.index)
-app.post("/message",router.message)
+app.get("/", router.index)
+app.post("/message", router.message)
 app.listen(port);
--- test/index.js
+++ test/index.js
@@ -1,44 +1,62 @@
+const server = require('../app.js')
 const msg = require('../api/messageboard.js')
 const assert = require('chai').assert
+const request = require('supertest')
 
-describe('test email format', function(){
-  it('should be a legal mail-address',()=>{
+const mockreq = { author: "peter", email: "sofjid@gmail.com", content: "hello", time: new Date().toDateString() }
+
+describe('test req and res', () => {
+  it('should return res as mockreq', function (done) {
+    request(server)
+      .post('/message')
+      .type('form')
+      .send(mockreq)
+      .expect(200, (err, res) => {
+        assert.notExists(err)
+        assert.deepEqual(res.body, mockreq)
+        done()
+      })
+  })
+})
+
+describe('test email format', function () {
+  it('should be a legal mail-address', () => {
     assert(msg.test_email('ano876@gmail.com'))
   })
-  it('should have "@" sign',function(){
-    assert.isFalse(msg.test_email("234sfdagmail.com"),'should include "@" sign')
+  it('should have "@" sign', function () {
+    assert.isFalse(msg.test_email("234sfdagmail.com"), 'should include "@" sign')
   })
-  it('should not contain special chracter',function(){
-    assert.isFalse(msg.test_email("@.."),'don\'t use special character')
+  it('should not contain special chracter', function () {
+    assert.isFalse(msg.test_email("@.."), 'don\'t use special character')
   })
-  it('should not enter a empty string',()=>{
+  it('should not enter a empty string', () => {
     assert.isFalse(msg.test_email("  "))
   })
-  it('should have character before and after "@" sign ',() => {
+  it('should have character before and after "@" sign ', () => {
     assert.isFalse(msg.test_email("@ssafsadf.casdf"))
     assert.isFalse(msg.test_email("sfasdf@"))
     assert.isFalse(msg.test_email("@"))
   })
 })
 
-describe('test the legal name',()=>{
-  it('should be a legal name',()=>{
+describe('test the legal name', () => {
+  it('should be a legal name', () => {
     assert(msg.test_name('kevin'))
   })
-  it('should be a string',()=>{
+  it('should be a string', () => {
     assert.isFalse(msg.test_name(2345))
   })
-  it('should not contain the dirty word',() => {
+  it('should not contain the dirty word', () => {
     assert.isFalse(msg.test_name('fuck'))
     assert.isFalse(msg.test_name('bitch'))
   })
-  it('should not a empty string',()=>{
+  it('should not a empty string', () => {
     assert.isFalse(msg.test_name(' '))
   })
-  it('should not contain number',()=>{
+  it('should not contain number', () => {
     assert.isFalse(msg.test_name('name456'))
   })
-  it('should not contain special character',()=>{
+  it('should not contain special character', () => {
     assert.isFalse(msg.test_name('`qqwr'))
     assert.isFalse(msg.test_name('_wr'))
     assert.isFalse(msg.test_name('@qq<r'))
@@ -52,8 +70,8 @@ describe('test the legal name',()=>{
   })
 })
 
-describe('filter the bad word',()=>{
-  it('should not contain the bad word',()=>{
+describe('filter the bad word', () => {
+  it('should not contain the bad word', () => {
     assert.isFalse(msg.test_content('what are you fuck doing'))
   })
 
